8f1c5293ae9de05be4c7d0c715b261e1f97a7f73
Shourya742 <sshourya17@gmail.com>
2023-07-21 20:33:35 +0530

    Added Contact

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,25 +1,116 @@
 import React from "react";
 import { styled } from "styled-components";
 
+const data = [
+  "Web Design",
+  "Development",
+  "Illustration",
+  "Product Design",
+  "Social Media",
+];
+
 const Section3 = styled.div`
   height: 100vh;
   scroll-snap-align: center;
   display: flex;
   justify-content: center;
+  position: relative;
+  color: black;
+  font-size: 14px;
+  font-weight: 300;
 `;
 
 const Container = styled.div`
   width: 1400px;
   display: flex;
   justify-content: space-between;
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    flex-direction: column;
+  }
 `;
-const Left = styled.div``;
-const List = styled.ul``;
-const ListItem = styled.li``;
-const Right = styled.div``;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+
+  @media only screen and (max-width: 768px) {
+    padding: 20px;
+    justify-content: center;
+  }
+`;
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const ListItem = styled.li`
+  font-size: 90px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+  position: relative;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 24px;
+    color: white;
+    -webkit-text-stroke: 0px;
+  }
+
+  ::after {
+    content: "${(props) => props.text}";
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: pink;
+    width: 0px;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  &:hover {
+    ::after {
+      animation: moveText 0.5s linear both;
+
+      @keyframes moveText {
+        to {
+          width: 100%;
+        }
+      }
+    }
+  }
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Works = () => {
-  return <Section3>Works</Section3>;
+  return (
+    <Section3>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item, index) => {
+              return (
+                <ListItem key={index} text={item}>
+                  {item}
+                </ListItem>
+              );
+            })}
+            <ListItem></ListItem>
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section3>
+  );
 };
 
 export default Works;
