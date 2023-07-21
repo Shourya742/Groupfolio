f9e67345d07d7af411c37ff1d8d7acf2c86faf05
Shourya742 <sshourya17@gmail.com>
2023-07-21 16:33:12 +0530

    Done with Hero and Who pages

---
+++ src/components/Works.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const Section3 = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Left = styled.div``;
+const List = styled.ul``;
+const ListItem = styled.li``;
+const Right = styled.div``;
+
+const Works = () => {
+  return <Section3>Works</Section3>;
+};
+
+export default Works;
